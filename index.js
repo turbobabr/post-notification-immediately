b0f3d0db169a8274e6b82a9bdbdc504064afa398
Andrey Shakhmin <andrey.shakhmin@gmail.com>
2018-12-05 11:23:15 +0800

    escaping and args validation

--- index.js
+++ index.js
@@ -7,23 +7,47 @@ function fixPathForAsarUnpack(path) {
   return isUsingAsar ? path.replace('app.asar', 'app.asar.unpacked') : path;
 }
 
+function isString(obj) {
+  return obj && toString.call(obj) === '[object String]';
+}
+
+function isObject(obj) {
+  return toString.call(obj) === '[object Object]';  
+}
+
 const postNotification = (name,userInfo,options) => {
   const electronFixedModuleBinaryPath = __dirname.endsWith('app.asar') ? 'node_modules/post-notification-immediately/bin' : 'bin'
   const BIN = options && options.execPath ? options.execPath : path.join(fixPathForAsarUnpack(__dirname), isElectron ? electronFixedModuleBinaryPath : 'bin');
+  return new Promise((resolve,reject) => {    
+    if(!isString(name)) {
+      reject(new Error("'name' argument should be a valid string!"));
+      return;
+    }
 
-  const binAndArgs = [BIN,JSON.stringify({
-    name: name,
-    userInfo: userInfo
-  })];
+    if(userInfo && !isObject(userInfo)) {
+      reject(new Error("'userInfo' argument should be empty or a valid object!"));
+      return;
+    }
 
-  return new Promise((resolve,reject) => {
-    exec(binAndArgs.join(" "),(err) => {
+    let execStr;
+    try {
+      // FIXME: Dirty way to escape quotes in strigified objects!
+      execStr = [BIN,JSON.stringify(JSON.stringify({
+        name: name,
+        payload: userInfo || {}
+      }))].join(" ");
+    } catch(e) {
+      reject(e);
+      return;
+    }
+    
+    exec(execStr,(err) => {
       if(err) {
           reject(err);
           return;
       }
 
-      resolve(json);      
+      resolve();      
     });
   });
 };
